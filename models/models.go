924adfe4769c9cddab2bf72f310f1495a9939a9f
Roshan Patil <patilroshan443@gmail.com>
2020-06-14 10:05:19 +0530

    Remove unused code

--- models/models.go
+++ models/models.go
@@ -1,15 +1,1 @@
 package models
-
-// // Employee - employee struct
-// type Employee struct {
-// 	ID      string  `json:"id,omitempty"`
-// 	Name    string  `json:"name,omitempty"`
-// 	Email   string  `json:"email,omitempty"`
-// 	Address Address `json:"address,omitempty"`
-// }
-
-// // Address - Address struct
-// type Address struct {
-// 	City    string `json:"city,omitempty"`
-// 	Pincode string `json:"pincode,omitempty"`
-// }
